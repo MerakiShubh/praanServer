e2561befcebab335418597ee5ffb50e2091b83a0
MerakiShubh <shubhamt.champ@gmail.com>
2024-10-28 21:38:22 +0530

    add corn configuration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+// src/index.js
 import { app } from "./app.js";
 import { config } from "./config/config.js";
 import connectDB from "./db/index.js";
 import mqttClient from "./mqtt/index.js";
+import cron from "node-cron";
+import { publishRandomData } from "./scripts/publishRandomData.js";
 
 const port = config.get("port");
 
 connectDB()
   .then(() => {
+    // Start the server
     app.listen(port || 3000, () => {
       console.log(`⚙️ Server is running at port : ${port}`);
     });
@@ -26,6 +30,12 @@ connectDB()
     mqttClient.on("message", (topic, message) => {
       console.log(`📥 Received message on ${topic}: ${message.toString()}`);
     });
+
+    // Schedule the cron job to publish random data every 20 seconds
+    // cron.schedule("*/20 * * * * *", () => {
+    //   console.log("Publishing random data to HiveMQ...");
+    //   publishRandomData();
+    // });
   })
   .catch((err) => {
     console.log("❌ MONGO DB connection failed!", err);
