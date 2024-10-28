9197cebe4dc8108f503d50903fba8ef4f16078ad
MerakiShubh <shubhamt.champ@gmail.com>
2024-10-28 21:39:22 +0530

    refreshtoken route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,11 @@
 import Router from "express";
-import { createAuthenticateUser } from "../controllers/user.controller.js";
+import {
+  createAuthenticateUser,
+  refreshAccessToken,
+} from "../controllers/user.controller.js";
 const router = Router();
 
 router.route("/createuser").post(createAuthenticateUser);
+router.route("/refreshtoken").post(refreshAccessToken);
 
 export default router;
